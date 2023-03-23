a0690df8bc7ef96a53f9d7cb0691d4b12772ae13
wangzhen <wanghzhenk@fotile.com>
2023-03-23 15:24:42 +0800

    1. 修改测试脚本支持数组

--- src/test.c
+++ src/test.c
@@ -11,30 +11,35 @@
 typedef unsigned char uint8_t;
 
 /* Step1:定义与json相对应的数据结构 */
-typedef struct {
+typedef struct
+{
     uint8_t offset;
     uint8_t len;
     uint8_t bit_index;
 } cmd_attr_t;
 
-typedef struct {
+typedef struct
+{
     uint8_t offset;
     uint8_t len;
 } sta_attr_t;
 
-typedef struct {
+typedef struct
+{
     char *attr;
     cmd_attr_t cmd;
     sta_attr_t status;
 } attri_list;
 
-typedef struct {
+typedef struct
+{
     uint8_t offset;
     uint8_t len;
     int default_;
 } cfg_attr_t;
 
-typedef struct {
+typedef struct
+{
     cfg_attr_t head;
     cfg_attr_t type;
     cfg_attr_t payloadBitFlag;
@@ -42,21 +47,24 @@ typedef struct {
     cfg_attr_t crc;
 } cmd_payload_cfg_t;
 
-typedef struct {
+typedef struct
+{
     cfg_attr_t payloadData;
 } sta_payload_cfg_t;
 
-typedef struct {
+typedef struct
+{
     sta_payload_cfg_t status;
     cmd_payload_cfg_t cmd;
 } header_info;
 
-typedef struct {
+typedef struct
+{
     char *version;
     uint8_t dataType;
     uint8_t uploadFilter;
     int attrNum;
-    attri_list *attributeList;
+    attri_list attributeList[30];
     header_info header;
 } fiks_schema_t;
 
@@ -65,13 +73,13 @@ reflect_item_t cmd_attr_t_ref_tbl[] = {
     _property_int2(cmd_attr_t, offset, "offset"),
     _property_int2(cmd_attr_t, len, "len"),
     _property_int(cmd_attr_t, bit_index),
-    _property_end()
+    _property_end(),
 };
 
 reflect_item_t sta_attr_t_ref_tbl[] = {
     _property_int2(sta_attr_t, offset, "offset"),
     _property_int2(sta_attr_t, len, "len"),
-    _property_end()
+    _property_end(),
 };
 
 /* description for attri_list */
@@ -79,14 +87,14 @@ reflect_item_t attri_list_ref_tbl[] = {
     _property_string2(attri_list, attr, "attr"),
     _property_obj2(attri_list, status, "status", sta_attr_t_ref_tbl),
     _property_obj2(attri_list, cmd, "cmd", cmd_attr_t_ref_tbl),
-    _property_end()
+    _property_end(),
 };
 
 reflect_item_t cfg_attr_t_ref_tbl[] = {
     _property_int2(cfg_attr_t, offset, "offset"),
     _property_int2(cfg_attr_t, len, "len"),
     _property_int2(cfg_attr_t, default_, "default"),
-    _property_end()
+    _property_end(),
 };
 
 reflect_item_t cmd_payload_cfg_t_ref_tbl[] = {
@@ -95,19 +103,19 @@ reflect_item_t cmd_payload_cfg_t_ref_tbl[] = {
     _property_obj2(cmd_payload_cfg_t, payloadBitFlag, "payloadBitFlag", cfg_attr_t_ref_tbl),
     _property_obj2(cmd_payload_cfg_t, payloadData, "payloadData", cfg_attr_t_ref_tbl),
     _property_obj2(cmd_payload_cfg_t, crc, "crc", cfg_attr_t_ref_tbl),
-    _property_end()
+    _property_end(),
 };
 
 reflect_item_t sta_payload_cfg_t_ref_tbl[] = {
     _property_obj2(sta_payload_cfg_t, payloadData, "payloadData", cfg_attr_t_ref_tbl),
-    _property_end()
+    _property_end(),
 };
 
 /* description for header_info */
 reflect_item_t header_info_ref_tbl[] = {
     _property_obj2(header_info, cmd, "cmd", cmd_payload_cfg_t_ref_tbl),
     _property_obj2(header_info, status, "status", sta_payload_cfg_t_ref_tbl),
-    _property_end()
+    _property_end(),
 };
 
 /* description for fiks_schema_t */
@@ -116,9 +124,9 @@ reflect_item_t fiks_schema_t_ref_tbl[] = {
     _property_int2(fiks_schema_t, dataType, "dataType"),
     _property_int2(fiks_schema_t, uploadFilter, "uploadFilter"),
     _property_int_ex2(fiks_schema_t, attrNum, "attrNum", _ex_args_all),
-    _property_array2(fiks_schema_t, attributeList, "AttributeList", attri_list_ref_tbl, attri_list, "attrNum"),
+    _property_arrays2(fiks_schema_t, attributeList, "AttributeList", attri_list_ref_tbl, attri_list, "attrNum"),
     _property_obj2(fiks_schema_t, header, "header", header_info_ref_tbl),
-    _property_end()
+    _property_end(),
 };
 
 void my_log(log_Event *ev)
@@ -127,8 +135,8 @@ void my_log(log_Event *ev)
     ftime(&stTimeb);
     char buf[1024];
     buf[strftime(buf, sizeof(buf), "[%Y-%m-%d %H:%M:%S", ev->time)] = '\0';
-    snprintf(buf + strlen(buf), sizeof(buf) - strlen(buf) - 1, ".%03d] %-5s %s::%s:%d: ",
-        stTimeb.millitm, log_level_string(ev->level), ev->file, ev->func, ev->line);
+    snprintf(buf + strlen(buf), sizeof(buf) - strlen(buf) - 1, ".%03d] %-5s %s::%s:%d: ", stTimeb.millitm,
+        log_level_string(ev->level), ev->file, ev->func, ev->line);
     vsnprintf(buf + strlen(buf), sizeof(buf) - strlen(buf) - 1, ev->fmt, ev->ap);
     printf("%s\n", buf);
 }
@@ -168,6 +176,8 @@ int csonDemo()
 
     clock_t begin = clock();
     fiks_schema_t gs_schema = {};
+    csonFreePointer(&gs_schema, fiks_schema_t_ref_tbl);
+
     memset(&gs_schema, 0, sizeof(fiks_schema_t));
 
     int ret = csonJsonStr2Struct(buf, &gs_schema, fiks_schema_t_ref_tbl);
@@ -175,7 +185,8 @@ int csonDemo()
     int cost = end - begin;
     printf("attrNum:%d\n", gs_schema.attrNum);
     for (int i = 0; i < gs_schema.attrNum; i++) {
-        attri_list *attrList = gs_schema.attributeList + i;
+        // attri_list *attrList = gs_schema.attributeList + i;
+        attri_list *attrList = &gs_schema.attributeList[i];
         printf("[%d] %s cmd[%d, %d, %d] status[%d, %d]\n", i, attrList->attr, attrList->cmd.offset, attrList->cmd.len,
             attrList->cmd.bit_index, attrList->status.offset, attrList->status.len);
     }
@@ -190,12 +201,14 @@ int csonDemo()
     printf("decode ret:%d cost: %dms\n", ret, cost);
 
     begin = clock();
-    char* jstrOutput = NULL;
+    char *jstrOutput = NULL;
     ret = csonStruct2JsonStr(&jstrOutput, &gs_schema, fiks_schema_t_ref_tbl);
     end = clock();
     cost = end - begin;
-    // printf("encode json:\n%s\n", jstrOutput);
+    printf("encode json:\n%s\n", jstrOutput);
     printf("encode ret: %d cost: %dms\n", ret, cost);
+
+    csonFreePointer(&gs_schema, fiks_schema_t_ref_tbl);
 
     if (jstrOutput != NULL) {
         free(jstrOutput);
@@ -206,8 +219,71 @@ int csonDemo()
         free(buf);
         buf = NULL;
     }
-    csonFreePointer(&gs_schema, fiks_schema_t_ref_tbl);
     return 0;
+}
+
+typedef struct
+{
+    int timeStamp;
+} ErrorInfo;
+
+reflect_item_t ErrorInfo_ref_tbl[] = {
+    _property_int(ErrorInfo, timeStamp),
+    _property_end(),
+};
+
+typedef struct
+{
+    char productId[128];
+    char productKey[128];
+    char securityKey[128];
+    char xSource[128];
+    char *productName;
+    int productSpecies;
+    long long timestamp;
+    ErrorInfo error;
+} ProdcutInfo;
+
+reflect_item_t ProdcutInfo_ref_tbl[] = {
+    _property_strings(ProdcutInfo, productId),
+    _property_strings(ProdcutInfo, productKey),
+    _property_strings(ProdcutInfo, securityKey),
+    _property_strings(ProdcutInfo, xSource),
+    _property_string(ProdcutInfo, productName),
+    _property_int(ProdcutInfo, productSpecies),
+    _property_int(ProdcutInfo, timestamp),
+    _property_obj(ProdcutInfo, error, ErrorInfo_ref_tbl),
+    _property_end(),
+};
+
+void csonDemo2()
+{
+    char *res_data = "{\"securityKey\":\"HqXpxKuXlrt1FIGu\",\"upgradeKey\":"
+                     "\"95880288201091325707433253118984263478572987735494687588750185795377577721630844788736994473060"
+                     "34466200616411960574122434059469100235892702736860872901247123456\",\"productId\":"
+                     "\"e27d0ebc8fccd79f05c9ce604237cb20\",\"productName\":\"iMES2.0iMES-45-C1\",\"productKey\":"
+                     "\"6e683d0bf8cc48a38ffc83d351be961f\",\"xSource\":\"DEVICE_iMES-45-C1\",\"productSpecies\":2,"
+                     "\"timestamp\":1678185441827}";
+    ProdcutInfo pdInfo = {
+        .productId = "",
+        .productSpecies = 1,
+        .error.timeStamp = -1,
+    };
+    int ret = csonJsonStr2Struct(res_data, &pdInfo, ProdcutInfo_ref_tbl);
+    printf("ret: %d productId: %s size:%d xSource: %s productSpecies: %d timeStamp: %lld\n", ret, 
+        pdInfo.productId, strlen(pdInfo.productId),
+        pdInfo.xSource,
+        pdInfo.productSpecies, pdInfo.timestamp);
+
+    char *jstrOutput = NULL;
+    ret = csonStruct2JsonStr(&jstrOutput, &pdInfo, ProdcutInfo_ref_tbl);
+    printf("encode ret: %d jstrOutput: %s\n", ret, jstrOutput);
+
+    if (jstrOutput != NULL) {
+        free(jstrOutput);
+        jstrOutput = NULL;
+    }
+    csonFreePointer(&pdInfo, ProdcutInfo_ref_tbl);
 }
 
 int main(int argc, char *argv[])
@@ -216,7 +292,9 @@ int main(int argc, char *argv[])
     printf("\t\tRunning %s\n", __FUNCTION__);
     printf("=========================================\n");
 
-    csonDemo();
+    // csonDemo();
+
+    csonDemo2();
 
     return 0;
 }
