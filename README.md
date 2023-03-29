ec0f8cec350be2b0d89e530b17139061594b5b4b
wangzhen <wanghzhenk@fotile.com>
2023-03-29 10:08:27 +0800

    add test code

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # test for cson
 add log from https://github.com/rxi/log.c
+
+valgrind --leak-check=yes --show-reachable=yes ./build/test-cson
--- src/main.c
+++ src/main.c
@@ -5,37 +5,40 @@
 #include <string.h>
 #include <sys/time.h>
 #include <unistd.h>
+#include <stdint.h>
 
 #include "cson.h"
 #include "log.h"
 
-typedef unsigned char uint8_t;
-
 /* Step1:定义与json相对应的数据结构 */
 typedef struct
 {
-    uint8_t offset;
-    uint8_t len;
-    uint8_t bit_index;
+    uint32_t offset; // payload偏移字节byte
+    uint32_t offset_bit; // payload偏移位bit
+    uint16_t len; // 长度字节byte
+    uint16_t len_bit; // 长度位bit
+    uint32_t bit_index; // 在payloadBitFlag
 } cmd_attr_t;
 
 typedef struct
 {
-    uint8_t offset;
-    uint8_t len;
+    uint32_t offset;
+    uint32_t offset_bit;
+    uint16_t len;
+    uint16_t len_bit;
 } sta_attr_t;
 
 typedef struct
 {
-    char *attr;
+    char *name;
     cmd_attr_t cmd;
     sta_attr_t status;
 } attri_list;
 
 typedef struct
 {
-    uint8_t offset;
-    uint8_t len;
+    uint32_t offset;
+    uint16_t len;
     int default_;
 } cfg_attr_t;
 
@@ -43,6 +46,10 @@ typedef struct
 {
     cfg_attr_t head;
     cfg_attr_t type;
+    cfg_attr_t len;
+    cfg_attr_t cmd;
+    cfg_attr_t stat_;
+    cfg_attr_t flags;
     cfg_attr_t payloadBitFlag;
     cfg_attr_t payloadData;
     cfg_attr_t crc;
@@ -55,8 +62,8 @@ typedef struct
 
 typedef struct
 {
-    sta_payload_cfg_t status;
     cmd_payload_cfg_t cmd;
+    sta_payload_cfg_t status;
 } header_info;
 
 typedef struct
@@ -65,68 +72,74 @@ typedef struct
     uint8_t dataType;
     uint8_t uploadFilter;
     int attrNum;
-    attri_list attributeList[30];
+    attri_list *attrList;
     header_info header;
+    int cmd_size;
 } fiks_schema_t;
+
 
 /* Step2:定义数据结构的反射表 */
 reflect_item_t cmd_attr_t_ref_tbl[] = {
-    _property_int2(cmd_attr_t, offset, "offset"),
-    _property_int2(cmd_attr_t, len, "len"),
+    _property_int(cmd_attr_t, offset),
+    _property_int(cmd_attr_t, len),
     _property_int(cmd_attr_t, bit_index),
     _property_end(),
 };
 
 reflect_item_t sta_attr_t_ref_tbl[] = {
-    _property_int2(sta_attr_t, offset, "offset"),
-    _property_int2(sta_attr_t, len, "len"),
+    _property_int(sta_attr_t, offset),
+    _property_int(sta_attr_t, len),
     _property_end(),
 };
 
 /* description for attri_list */
 reflect_item_t attri_list_ref_tbl[] = {
-    _property_string2(attri_list, attr, "attr"),
-    _property_obj2(attri_list, status, "status", sta_attr_t_ref_tbl),
-    _property_obj2(attri_list, cmd, "cmd", cmd_attr_t_ref_tbl),
+    _property_string2(attri_list, name, "attr"),
+    _property_obj(attri_list, status, sta_attr_t_ref_tbl),
+    _property_obj(attri_list, cmd, cmd_attr_t_ref_tbl),
     _property_end(),
 };
 
 reflect_item_t cfg_attr_t_ref_tbl[] = {
-    _property_int2(cfg_attr_t, offset, "offset"),
-    _property_int2(cfg_attr_t, len, "len"),
+    _property_int(cfg_attr_t, offset),
+    _property_int(cfg_attr_t, len),
     _property_int2(cfg_attr_t, default_, "default"),
     _property_end(),
 };
 
 reflect_item_t cmd_payload_cfg_t_ref_tbl[] = {
-    _property_obj2(cmd_payload_cfg_t, head, "head", cfg_attr_t_ref_tbl),
-    _property_obj2(cmd_payload_cfg_t, type, "type", cfg_attr_t_ref_tbl),
-    _property_obj2(cmd_payload_cfg_t, payloadBitFlag, "payloadBitFlag", cfg_attr_t_ref_tbl),
-    _property_obj2(cmd_payload_cfg_t, payloadData, "payloadData", cfg_attr_t_ref_tbl),
-    _property_obj2(cmd_payload_cfg_t, crc, "crc", cfg_attr_t_ref_tbl),
+    _property_obj(cmd_payload_cfg_t, head, cfg_attr_t_ref_tbl),
+    _property_obj(cmd_payload_cfg_t, type, cfg_attr_t_ref_tbl),
+    _property_obj(cmd_payload_cfg_t, len, cfg_attr_t_ref_tbl),
+    _property_obj(cmd_payload_cfg_t, cmd, cfg_attr_t_ref_tbl),
+    _property_obj2(cmd_payload_cfg_t, stat_, "stat", cfg_attr_t_ref_tbl),
+    _property_obj(cmd_payload_cfg_t, flags, cfg_attr_t_ref_tbl),
+    _property_obj(cmd_payload_cfg_t, payloadBitFlag, cfg_attr_t_ref_tbl),
+    _property_obj(cmd_payload_cfg_t, payloadData, cfg_attr_t_ref_tbl),
+    _property_obj(cmd_payload_cfg_t, crc, cfg_attr_t_ref_tbl),
     _property_end(),
 };
 
 reflect_item_t sta_payload_cfg_t_ref_tbl[] = {
-    _property_obj2(sta_payload_cfg_t, payloadData, "payloadData", cfg_attr_t_ref_tbl),
+    _property_obj(sta_payload_cfg_t, payloadData, cfg_attr_t_ref_tbl),
     _property_end(),
 };
 
 /* description for header_info */
 reflect_item_t header_info_ref_tbl[] = {
-    _property_obj2(header_info, cmd, "cmd", cmd_payload_cfg_t_ref_tbl),
-    _property_obj2(header_info, status, "status", sta_payload_cfg_t_ref_tbl),
+    _property_obj(header_info, cmd, cmd_payload_cfg_t_ref_tbl),
+    _property_obj(header_info, status, sta_payload_cfg_t_ref_tbl),
     _property_end(),
 };
 
 /* description for fiks_schema_t */
 reflect_item_t fiks_schema_t_ref_tbl[] = {
-    _property_string2(fiks_schema_t, version, "version"),
-    _property_int2(fiks_schema_t, dataType, "dataType"),
-    _property_int2(fiks_schema_t, uploadFilter, "uploadFilter"),
-    _property_int_ex2(fiks_schema_t, attrNum, "attrNum", _ex_args_all),
-    _property_arrays2(fiks_schema_t, attributeList, "AttributeList", attri_list_ref_tbl, attri_list, "attrNum"),
-    _property_obj2(fiks_schema_t, header, "header", header_info_ref_tbl),
+    _property_string(fiks_schema_t, version),
+    _property_int(fiks_schema_t, dataType),
+    _property_int(fiks_schema_t, uploadFilter),
+    _property_int_ex(fiks_schema_t, attrNum, _ex_args_all),
+    _property_array2(fiks_schema_t, attrList, "AttributeList", attri_list_ref_tbl, attri_list, "attrNum"),
+    _property_obj(fiks_schema_t, header, header_info_ref_tbl),
     _property_end(),
 };
 
@@ -184,9 +197,9 @@ int csonDemo()
     int cost = end - begin;
     log_debug("attrNum:%d", gs_schema.attrNum);
     for (int i = 0; i < gs_schema.attrNum; i++) {
-        // attri_list *attrList = gs_schema.attributeList + i;
-        attri_list *attrList = &gs_schema.attributeList[i];
-        log_debug("[%d] %s cmd[%d, %d, %d] status[%d, %d]", i, attrList->attr, attrList->cmd.offset, attrList->cmd.len,
+        attri_list *attrList = gs_schema.attrList + i;
+        // attri_list *attrList = &gs_schema.attrList[i];
+        log_debug("[%d] %s cmd[%d, %d, %d] status[%d, %d]", i, attrList->name, attrList->cmd.offset, attrList->cmd.len,
             attrList->cmd.bit_index, attrList->status.offset, attrList->status.len);
     }
 
@@ -299,9 +312,127 @@ void csonDemo2()
     csonFreePointer(&pdInfo, ProdcutInfo_ref_tbl);
 }
 
+void DumpIndevSchema(fiks_schema_t *schema)
+{
+    for (int i = 0; i < schema->attrNum; i++) {
+        attri_list *attr_info = &schema->attrList[i];
+        log_debug("[%d] attr:%s cmd[of:%d of_bit:%d len:%d len_bit:%d bit_idx:%d] "
+              "status[of:%d of_bit:%d len:%d len_b:%d]",
+            i, attr_info->name, attr_info->cmd.offset, attr_info->cmd.offset_bit, attr_info->cmd.len,
+            attr_info->cmd.len_bit, attr_info->cmd.bit_index, attr_info->status.offset, attr_info->status.offset_bit,
+            attr_info->status.len, attr_info->status.len_bit);
+    }
+}
+
+void wireless_indoor_schema_convert(fiks_schema_t *schema)
+{
+    for (int i = 0; i < schema->attrNum - 1; i++) {
+        for (int j = 0; j < schema->attrNum - 1 - i; j++) {
+            attri_list *attr_info1 = &schema->attrList[j];
+            attri_list *attr_info2 = &schema->attrList[j + 1];
+            if (attr_info1->status.offset > attr_info2->status.offset) {
+                attri_list schema_config_temp = {};
+                memcpy(&schema_config_temp, attr_info1, sizeof(schema_config_temp));
+                memcpy(attr_info1, attr_info2, sizeof(schema_config_temp));
+                memcpy(attr_info2, &schema_config_temp, sizeof(schema_config_temp));
+            }
+        }
+    }
+
+    log_info("after convert schema_config from small to big ~~~~");
+
+    DumpIndevSchema(schema);
+
+    for (int i = 0; i < schema->attrNum - 1; i++) {
+        attri_list *attr_info1 = &schema->attrList[i];
+        attri_list *attr_info2 = &schema->attrList[i + 1];
+        int report_bit_len1 = attr_info1->status.len * 8 + attr_info1->status.len_bit;
+        if (report_bit_len1 >= 8) {
+            attr_info2->status.offset = attr_info1->status.offset + (report_bit_len1 / 8);
+        } else if (report_bit_len1 == 1) {
+            int report_bit_len2 = attr_info2->status.len * 8 + attr_info2->status.len_bit;
+            if (report_bit_len2 == 1) {
+                attr_info2->status.offset = attr_info1->status.offset;
+            } else if (report_bit_len2 == 8) {
+                attr_info2->status.offset = attr_info1->status.offset + 1;
+            }
+        }
+    }
+
+    log_info("after remove the attr[workmode] index from schema_config ~~~~");
+
+    DumpIndevSchema(schema);
+}
+
+void init_things_config(fiks_schema_t *schema)
+{
+    log_debug("attrNum: %d", schema->attrNum);
+    for (int i = 0; i < schema->attrNum; i++) {
+        attri_list *attr_info = schema->attrList + i;
+        if (schema->dataType == 1) {  // 如果是按位表示, 则转换位按字节表示
+            attr_info->cmd.offset_bit = attr_info->cmd.offset % 8;
+            attr_info->cmd.offset /= 8;
+            attr_info->cmd.len_bit = attr_info->cmd.len % 8;
+            attr_info->cmd.len /= 8;
+
+            attr_info->status.offset_bit = attr_info->status.offset % 8;
+            attr_info->status.offset /= 8;
+            attr_info->status.len_bit = attr_info->status.len % 8;
+            attr_info->status.len /= 8;
+        }
+        log_debug("[%d] attr:%s cmd[of:%d of_bit:%d len:%d len_bit:%d bit_idx:%d] "
+              "status[of:%d of_bit:%d len:%d len_b:%d]",
+            i, attr_info->name, attr_info->cmd.offset, attr_info->cmd.offset_bit, attr_info->cmd.len,
+            attr_info->cmd.len_bit, attr_info->cmd.bit_index, attr_info->status.offset, attr_info->status.offset_bit,
+            attr_info->status.len, attr_info->status.len_bit);
+    }
+
+    cmd_payload_cfg_t *head_cmd = &schema->header.cmd;
+    log_debug("head_cmd head[%d, %d, %d]", head_cmd->head.offset, head_cmd->head.len, head_cmd->head.default_);
+    log_debug("head_cmd type[%d, %d, %d]", head_cmd->type.offset, head_cmd->type.len, head_cmd->type.default_);
+    log_debug("head_cmd cmd[%d, %d, %d]", head_cmd->cmd.offset, head_cmd->cmd.len, head_cmd->cmd.default_);
+    log_debug("head_cmd payloadBitFlag[%d, %d]", head_cmd->payloadBitFlag.offset, head_cmd->payloadBitFlag.len);
+    log_debug("head_cmd payloadData[%d, %d]", head_cmd->payloadData.offset, head_cmd->payloadData.len);
+    log_debug("head_cmd crc[%d, %d]", head_cmd->crc.offset, head_cmd->crc.len);
+
+    schema->cmd_size = head_cmd->crc.offset + head_cmd->crc.len;
+
+    sta_payload_cfg_t *head_status = &schema->header.status;
+    log_debug("head_status payloadData[%d, %d]", head_status->payloadData.offset, head_status->payloadData.len);
+}
+
+void csonDemo3()
+{
+    char *buf =
+        "{\"AttributeList\":[{\"attr\":\"InnerRoomNum\",\"status\":{\"offset\":40,\"len\":8}},{\"attr\":"
+        "\"InnerFloorNum\",\"status\":{\"offset\":48,\"len\":8}},{\"attr\":\"PowerSwitchAll\",\"status\":{\"offset\":"
+        "56,\"len\":8}},{\"attr\":\"ElectricValveAngle\",\"cmd\":{\"offset\":72,\"len\":8,\"bit_idx\":0},\"status\":{"
+        "\"offset\":72,\"len\":8}},{\"attr\":\"FanGears\",\"status\":{\"offset\":80,\"len\":8}},{\"attr\":"
+        "\"InnerFanPower\",\"status\":{\"offset\":88,\"len\":16}},{\"attr\":\"InnerFanCommError\",\"status\":{"
+        "\"offset\":105,\"len\":1}},{\"attr\":\"InnerFanValveRunError\",\"status\":{\"offset\":108,\"len\":1}},{"
+        "\"attr\":\"InnerFanDialError\",\"status\":{\"offset\":111,\"len\":1}},{\"attr\":\"EndAttrOffset\",\"status\":{"
+        "\"offset\":112,\"len\":8}},{\"attr\":\"SignalStrength\",\"status\":{\"offset\":0,\"len\":0}},{\"attr\":"
+        "\"RebootCause\",\"status\":{\"offset\":0,\"len\":0}},{\"attr\":\"ErrorMessageRecord\",\"status\":{\"offset\":"
+        "0,\"len\":0}}],\"version\":\"V1.0.0_bit\",\"dataType\":1,\"uploadFilter\":1,\"header\":{\"cmd\":{\"head\":{"
+        "\"offset\":0,\"len\":2,\"default\":62709},\"len\":{\"offset\":2,\"len\":1},\"type\":{\"offset\":4,\"len\":1,"
+        "\"default\":195},\"cmd\":{\"offset\":3,\"len\":1,\"default\":2},\"stat\":{\"offset\":0,\"len\":0,\"default\":"
+        "0},\"flags\":{\"offset\":0,\"len\":0,\"default\":0},\"payloadBitFlag\":{\"offset\":0,\"len\":0},"
+        "\"payloadData\":{\"offset\":5,\"len\":16},\"crc\":{\"offset\":21,\"len\":2}},\"status\":{\"payloadData\":{"
+        "\"offset\":5,\"len\":16}}}}";
+
+    fiks_schema_t schema = {};
+    memset(&schema, 0, sizeof(fiks_schema_t));
+    int ret = csonJsonStr2Struct(buf, &schema, fiks_schema_t_ref_tbl);
+
+    init_things_config(&schema);
+
+    wireless_indoor_schema_convert(&schema);
+
+    csonFreePointer(&schema, fiks_schema_t_ref_tbl);
+}
+
 extern void test1();
 extern void test2();
-
 int main(int argc, char *argv[])
 {
     log_info("=========================================");
@@ -313,13 +444,15 @@ int main(int argc, char *argv[])
 
     int cnt = 1;
     while (cnt-- > 0) {
-        csonDemo();
+        // csonDemo();
 
-        csonDemo2();
+        // csonDemo2();
 
-        test1();
+        csonDemo3();
 
-        test2();
+        // test1();
+
+        // test2();
 
         usleep(100 * 1000);
     }
